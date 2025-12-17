4b9287cead37c481b335d394627c43d607028635
«Anton» <tohakrasava02@gmail.com>
2025-12-17 17:14:42 +0200

    yuhu

--- index.py
+++ index.py
@@ -1,4 +1,2 @@
-print(748765 + 27956294657)
-print(234876952964367537275 - 456652174539254)
-print('dfsjkfhvdshgiouhocchkokdhfshijsfhsiauop')
-
+print('sdkjflghgvggbnggggwlgdkgseruyhw;qae')
+print(67325285326784)
