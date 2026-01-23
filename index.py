4b672432aca9073e159afe87d151f63f45a526cd
«Anton» <dlatelevuzora@gmail.com>
2026-01-23 21:11:30 +0200

    au

--- index.py
+++ index.py
@@ -4,7 +4,10 @@ print('Anton')
 print('Roma')
 print('jawa')
 print('wsgit')
-for i in range(20,100):
+# for i in range(20,100):
+#     print(i)
+# for i in range(22):
+#     print('Anton')
+
+for i in range(67, 167):
     print(i)
-for i in range(22):
-    print('Anton')
